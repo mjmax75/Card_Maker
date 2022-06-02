11ce0dce19e0d825fd96ebbc0c3bec525a3815fa
mjmax75 <minji.shon.415@gmail.com>
2022-06-02 15:52:54 -0500

    add login component

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import styles from "./footer.module.css";
 
-const Footer = (props) => <h1>Footer</h1>;
+const Footer = (props) => (
+  <footer className={styles.footer}>
+    <p className={styles.title}>Minji Shon</p>
+  </footer>
+);
 
 export default Footer;

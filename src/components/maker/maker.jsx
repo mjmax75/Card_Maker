4d379b92b9e555d68b3bc52b26d6619c6220a599
mjmax75 <minji.shon.415@gmail.com>
2022-06-03 14:28:37 -0500

    handle state change on editor

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,8 +7,8 @@ import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 
 const Maker = ({ authService }) => {
-  const [cards, setCards] = useState([
-    {
+  const [cards, setCards] = useState({
+    1: {
       id: "1",
       name: "Minji",
       company: "Shop Smart LLC",
@@ -19,7 +19,7 @@ const Maker = ({ authService }) => {
       fileName: "minji",
       fileURL: null,
     },
-    {
+    2: {
       id: "2",
       name: "Minji",
       company: "Shop Smart LLC",
@@ -30,7 +30,8 @@ const Maker = ({ authService }) => {
       fileName: "minji",
       fileURL: "minji.png",
     },
-    {
+
+    3: {
       id: "3",
       name: "Minji",
       company: "Shop Smart LLC",
@@ -41,7 +42,8 @@ const Maker = ({ authService }) => {
       fileName: "minji",
       fileURL: null,
     },
-  ]);
+  });
+
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -55,16 +57,32 @@ const Maker = ({ authService }) => {
     });
   });
 
-  const addCard = (card) => {
-    const updated = [...cards, card];
-    setCards(updated);
+  const createOrUpdateCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
+  };
+
+  const deleteCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
   };
 
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor
+          cards={cards}
+          addCard={createOrUpdateCard}
+          updateCard={createOrUpdateCard}
+          deleteCard={deleteCard}
+        />
         <Preview cards={cards} />
       </div>
       <Footer />
